f3c2b5e94c9d07cd56a5d716ad2832312bd684e3
dobler22 <jordirr22@gmail.com>
2026-06-03 21:31:18 -0500

    Cambios

--- Hotel APM Grand/Form1.cs
+++ Hotel APM Grand/Form1.cs
@@ -1,3 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
 namespace Hotel_APM_Grand
 {
     public partial class Form1 : Form
